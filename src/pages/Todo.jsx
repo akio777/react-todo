3064dcc87d27d6e7808ea510991416e340f4b899
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2021-01-21 20:48:23 +0700

    init Todo page

---
+++ src/pages/Todo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Todo() {
+  return (
+    <div className="bg-gray-700 min-h-screen flex justify-center items-center">
+      <h1 className="text-white text-5xl">todopage</h1>
+    </div>
+  )
+}
