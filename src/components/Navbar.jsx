a30ece436fdb390f51a9de3bf378b216d01cec9f
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2021-01-21 20:20:45 +0700

    update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-
 const Navbar = () => {
   return (
     <div>
-      <nav className="bg-gray-800 z-50 absolute min-w-full">
+      <nav className="bg-gray-800 z-50 fixed min-w-full">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-center h-16">
             <div className="flex items-center">
               <div className="flex items-baseline space-x-4">
-                {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
                 <NavLink exact to="/"
-                  activeClassName="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  activeClassName="bg-black bg-opacity-40 text-white px-3 py-2 rounded-md text-sm font-medium"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</NavLink>
                 <NavLink to="/todo"
-                  activeClassName="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  activeClassName="bg-black bg-opacity-40 text-white px-3 py-2 rounded-md text-sm font-medium"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Todo</NavLink>
               </div>
             </div>
