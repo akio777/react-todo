21a143702dcac0e16c5c60fabbe591fcb23aefe1
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2021-01-21 20:50:31 +0700

    update noselect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
           <Link to="/todo" className="text-white text-xl font-bold flex text-center items-center justify-center rounded-lg bg-blue-500 w-10 h-10 transition-all hover:bg-blue-600">→</Link>
         </div>
       </div>
-      <img src={logo} alt="React-logo" className="App-logo absolute z-0 opacity-10"/>
+      <img src={logo} alt="React-logo" className="App-logo absolute z-0 opacity-10 noselect"/>
       <p className="text-sm text-white absolute bottom-10 text-center font-light flex items-center noselect">Created with&nbsp;
       <a href="https://reactjs.org/" className="text-blue-400 underline font-normal transition-all hover:text-blue-500">React</a>&nbsp;and&nbsp;
       <a href="https://tailwindcss.com/" className="text-blue-400 underline font-normal transition-all hover:text-blue-500">tailwindcss</a>&nbsp;©&nbsp;<img src={MBLogo} alt="MB-Logo" className="mb-1 max-h-5 mb-logo" /></p>
