900a23164d523356e740d1a60c56c1f8844c2772
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2021-01-22 17:34:00 +0700

    update

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -21,7 +21,7 @@ export default function Todo() {
           <input type="text" className="todo flex-auto bg-gray-800 px-4 py-2 rounded-md text-white border-gray-300 focus:outline-none" placeholder="Enter todo task..." />
           <button className="add-btn flex bg-blue-500 px-4 py-2 ml-2 rounded-md text-white transition-all hover:bg-blue-600 focus:outline-none">Add</button>
         </div>
-        <h1 className="flex mt-6 text-xl text-white ">Todo List</h1>
+        <h1 className="flex mt-6 text-xl text-white noselect">Todo List</h1>
         <div className="list-container mt-6 w-2/4">
           {a.map((ele, i) => <List str={ele.str} finished={ele.finished} />)}
         </div>
