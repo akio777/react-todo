8b8c43d6923b73509617e9bab485c75c40b351aa
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2021-01-31 20:36:43 +0700

    add toggleFinish()

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function List({str, finished}) {
+export default function List({str}) {
+  const [finished, setFinished] = useState(false)
+
+  function toggleFinish() {
+    if (finished) {
+      setFinished(false)
+    }
+    else setFinished(true)
+  }
+
   return (
     <div className={ finished ? "mb-2 px-4 py-2 flex bg-gray-800 text-green-500 rounded-md cursor-pointer transition-all hover:bg-gray-600":
       "mb-2 px-4 py-2 flex bg-gray-800 text-white rounded-md cursor-pointer transition-all hover:bg-gray-600"}
-    title="Mark as Done">
+    title={finished ? "Mark as Undone" : "Mark as Done"} onClick={toggleFinish}>
       <h1 className="noselect max-w-full flex-auto overflow-ellipsis whitespace-nowrap">{finished ? "✔ "+str : str}</h1>
       <button className="text-gray-500 transition-all hover:text-red-600" title="Delete task">Del</button>
     </div>
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -5,33 +5,22 @@ export default function Todo() {
   const [input, setInput] = useState("")
   const [list, setList] = useState([])
   const inputRef = useRef(null)
-  // const a = [
-  //   { str: "Excercise", finished: true },
-  //   { str: "Study", finished: false },
-  //   { str: "Reading", finished: false },
-  //   { str: "Play", finished: true },
-  //   { str: "Study", finished: false },
-  //   { str: "Reading", finished: false },
-  //   { str: "Play", finished: true },
-  //   { str: "Study", finished: false },
-  //   { str: "Reading", finished: false },
-  //   { str: "Play", finished: true },
-  // ]
   function addList(e) {
     e.preventDefault()
-    setList([...list, { str: inputRef.current.value, finished: false }])
+    setList([...list, inputRef.current.value])
     setInput("")
   }
+
   return (
     <div className="bg-gray-700 min-h-screen">
       <div className="wrapper flex flex-col flex-wrap justify-center items-center">
         <form className="input-group flex w-2/4 mt-24" onSubmit={e => addList(e)}>
-          <input type="text" ref={inputRef} value={input} className="todo flex-auto bg-gray-800 px-4 py-2 rounded-md text-white border-gray-300 focus:outline-none" placeholder="Enter todo task..." onInput={e => setInput(e.target.value)} />
+          <input type="text" ref={inputRef} value={input} className="todo flex-auto bg-gray-800 px-4 py-2 rounded-md text-white border-gray-300 focus:outline-none" placeholder="Enter todo task..." onInput={() => setInput(inputRef.current.value)} required/>
           <button className="add-btn flex bg-blue-500 px-4 py-2 ml-2 rounded-md text-white transition-all hover:bg-blue-600 focus:outline-none">Add</button>
         </form>
         <h1 className="flex mt-6 text-xl text-white noselect">Todo List</h1>
         <div className="list-container mt-6 w-2/4">
-          {list.map((ele, i) => <List str={ele.str} finished={ele.finished} key={i}/>)}
+          {list.map((ele, i) => <List str={ele} key={i}/>)}
         </div>
       </div>
 
