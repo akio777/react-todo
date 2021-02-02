099f5ddae3c3343c5bfe90e626886c57f714d86b
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2021-02-02 17:38:05 +0700

    update Done style

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,21 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-export default function List({str}) {
-  const [finished, setFinished] = useState(false)
-
-  function toggleFinish() {
-    if (finished) {
-      setFinished(false)
-    }
-    else setFinished(true)
-  }
-
+export default function List({ obj, deleteList, toggleDone }) {
   return (
-    <div className={ finished ? "mb-2 px-4 py-2 flex bg-gray-800 text-green-500 rounded-md cursor-pointer transition-all hover:bg-gray-600":
-      "mb-2 px-4 py-2 flex bg-gray-800 text-white rounded-md cursor-pointer transition-all hover:bg-gray-600"}
-    title={finished ? "Mark as Undone" : "Mark as Done"} onClick={toggleFinish}>
-      <h1 className="noselect max-w-full flex-auto overflow-ellipsis whitespace-nowrap">{finished ? "✔ "+str : str}</h1>
-      <button className="text-gray-500 transition-all hover:text-red-600" title="Delete task">Del</button>
+    <div className="mb-2 px-4 py-2 flex bg-gray-800 text-white rounded-md cursor-pointer transition-all hover:bg-gray-600"
+      title={obj.done ? "Mark as Undone" : "Mark as Done"} onClick={toggleDone}>
+      <h1 className={obj.done ? "noselect max-w-full flex-auto overflow-ellipsis line-through text-green-500 whitespace-nowrap" : "noselect max-w-full flex-auto overflow-ellipsis whitespace-nowrap"}>{obj.str}</h1>
+      <button className="text-gray-500 transition-all hover:text-red-600" title="Delete task" onClick={(e) => { e.stopPropagation(); deleteList() }}>Del</button>
     </div>
   )
 }
